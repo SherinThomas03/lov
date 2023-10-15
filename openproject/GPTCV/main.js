d042cafe5765ba8849bed3a5104c24a32c63a38f
Sherin Thomas <48170445+SherinThomas03@users.noreply.github.com>
2023-10-15 17:10:03 +0530

    Update main.js

--- openproject/GPTCV/main.js
+++ openproject/GPTCV/main.js
@@ -17,27 +17,3 @@ function closeMenu() {
     hamburger.classList.remove("active");
     navMenu.classList.remove("active");
 }
-
-//for login :-)
-document.addEventListener("DOMContentLoaded", function() {
-const usernameInput = document.getElementById("username");
-const passwordInput = document.getElementById("password");
-const loginButton = document.querySelector("button");
-
-loginButton.addEventListener("click", function(event) {
-event.preventDefault();
-
-const correctUsername = "123"; 
-const correctPassword = "123"; 
-
-if (
-usernameInput.value === correctUsername &&
-passwordInput.value === correctPassword
-) {
-window.location.href = "openproject/GPTCV/index.html";
-}
-else {
-alert("Incorrect username or password. Please try again.");
-}
-});
-});
