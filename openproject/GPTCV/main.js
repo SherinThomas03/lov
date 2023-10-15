d87fd04c26008f2e57592fc8e7c608303e90164e
Sherin Thomas <48170445+SherinThomas03@users.noreply.github.com>
2023-10-15 15:36:07 +0530

    Add files via upload

---
+++ openproject/GPTCV/main.js
@@ -0,0 +1,43 @@
+//for harmburger :-)
+const hamburger = document.querySelector(".hamburgermenu");
+const navMenu = document.querySelector(".navigation-menu");
+
+hamburger.addEventListener("click", mobileMenu);
+
+function mobileMenu() {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+}
+
+const navLink = document.querySelectorAll(".navigation-link");
+
+navLink.forEach(n => n.addEventListener("click", closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove("active");
+    navMenu.classList.remove("active");
+}
+
+//for login :-)
+document.addEventListener("DOMContentLoaded", function() {
+const usernameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const loginButton = document.querySelector("button");
+
+loginButton.addEventListener("click", function(event) {
+event.preventDefault();
+
+const correctUsername = "123"; 
+const correctPassword = "123"; 
+
+if (
+usernameInput.value === correctUsername &&
+passwordInput.value === correctPassword
+) {
+window.location.href = "index.html";
+}
+else {
+alert("Incorrect username or password. Please try again.");
+}
+});
+});
