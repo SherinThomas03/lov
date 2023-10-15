a08e74482198b91e239c229b9ff1246fe742874a
Sherin Thomas <48170445+SherinThomas03@users.noreply.github.com>
2023-10-15 16:32:41 +0530

    Update main.js

--- openproject/GPTCV/main.js
+++ openproject/GPTCV/main.js
@@ -1,22 +1,4 @@
 //for harmburger :-)
-const hamburger = document.querySelector(".hamburgermenu");
-const navMenu = document.querySelector(".navigation-menu");
-
-hamburger.addEventListener("click", mobileMenu);
-
-function mobileMenu() {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
-}
-
-const navLink = document.querySelectorAll(".navigation-link");
-
-navLink.forEach(n => n.addEventListener("click", closeMenu));
-
-function closeMenu() {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
-}
 
 //for login :-)
 document.addEventListener("DOMContentLoaded", function() {
